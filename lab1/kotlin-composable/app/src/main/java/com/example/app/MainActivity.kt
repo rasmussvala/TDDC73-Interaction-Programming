2068eb939f237f543671d0e04b80acb670692276
Rasmus Svala <rasmus.svala@hotmail.com>
2024-11-07 14:01:53 +0100

    removed unused import
    Co-Authored-By: Grantallkotten <143391787+Grantallkotten@users.noreply.github.com>

--- lab1/kotlin-composable/app/src/main/java/com/example/app/MainActivity.kt
+++ lab1/kotlin-composable/app/src/main/java/com/example/app/MainActivity.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
