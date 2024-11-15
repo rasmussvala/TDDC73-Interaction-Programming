6a7b4a595708cb0d95c9cae8e06fc5a73604636a
Rasmus Svala <rasmus.svala@hotmail.com>
2024-11-15 14:54:57 +0100

    created a simple picker component

---
+++ lab2/app/PickerComponent.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { View, Button, Text } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+
+const PickerComponent: React.FC = () => {
+  const [selectedLanguage, setSelectedLanguage] = useState<string>("java");
+  const [isPickerOpen, setIsPickerOpen] = useState<boolean>(false);
+
+  function open() {
+    setIsPickerOpen(true);
+  }
+
+  function close() {
+    setIsPickerOpen(false);
+  }
+
+  return (
+    <View>
+      <Text>Selected Language: {selectedLanguage}</Text>
+
+      <Button
+        title={isPickerOpen ? "Close Picker" : "Open Picker"}
+        onPress={isPickerOpen ? close : open}
+      />
+
+      {isPickerOpen && (
+        <Picker
+          selectedValue={selectedLanguage}
+          onValueChange={(itemValue) => setSelectedLanguage(itemValue)}
+        >
+          <Picker.Item label="Java" value="java" />
+          <Picker.Item label="JavaScript" value="js" />
+        </Picker>
+      )}
+    </View>
+  );
+};
+
+export default PickerComponent;
