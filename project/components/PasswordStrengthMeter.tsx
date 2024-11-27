4a25ec3bbdc2aa684a266b945486c6b160f93081
Rasmus Svala <rasmus.svala@hotmail.com>
2024-11-27 11:31:50 +0100

    added status bar
    basic styling
    
    Co-Authored-By: Grantallkotten <143391787+Grantallkotten@users.noreply.github.com>

--- project/components/PasswordStrengthMeter.tsx
+++ project/components/PasswordStrengthMeter.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { View, TextInput, StyleSheet } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 
-const confirmPasswordWidget = () => {
+const ConfirmPasswordWidget = () => {
   const [confirmPassword, setconfirmPassword] = useState("");
 
   return (
@@ -18,10 +18,10 @@ const confirmPasswordWidget = () => {
 
 const PasswordStrengthMeter = () => {
   const [password, setPassword] = useState("");
-  const checkconfirmPassword = true;
+  const checkconfirmPassword = false;
 
   return (
-    <View>
+    <View style={styles.wrapper}>
       <View>
         <TextInput
           style={styles.input}
@@ -30,18 +30,57 @@ const PasswordStrengthMeter = () => {
           onChangeText={setPassword}
         />
       </View>
-      {checkconfirmPassword && confirmPasswordWidget()}
+      <View>
+        <View style={styles.status}>
+          <View style={styles.statusItem}></View>
+          <View style={styles.statusItem}></View>
+          <View style={styles.statusItem}></View>
+          <View style={styles.statusItem}></View>
+        </View>
+        <View style={styles.statusTextContainer}>
+          <Text style={styles.statusText}>Strong</Text>
+        </View>
+      </View>
+      {checkconfirmPassword && <ConfirmPasswordWidget />}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  wrapper: {
+    padding: 4,
+  },
+
   input: {
     height: 40,
-    margin: 12,
     borderWidth: 1,
     borderRadius: 8,
     padding: 10,
+  },
+
+  status: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: 4,
+  },
+
+  statusItem: {
+    backgroundColor: "lightgray",
+    flex: 1,
+    marginTop: 4,
+    borderRadius: 10,
+  },
+
+  statusTextContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+
+  statusText: {
+    fontSize: 12,
+    color: "gray",
   },
 });
 
