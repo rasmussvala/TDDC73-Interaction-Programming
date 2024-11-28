fcc704180c5e91bdb2e7a41c4da9d8e59479ed69
Rasmus Svala <rasmus.svala@hotmail.com>
2024-11-28 22:14:50 +0100

    added images in index

--- project/app/index.tsx
+++ project/app/index.tsx
@@ -1,8 +1,19 @@
 import { View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import PasswordStrengthMeter from "@/components/PasswordStrengthMeter";
+import Carousel from "@/components/Carousel";
 
 export default function Index() {
+  const images = [
+    require("../assets/images/carousel/1.png"),
+    require("../assets/images/carousel/2.png"),
+    require("../assets/images/carousel/3.png"),
+    require("../assets/images/carousel/4.png"),
+    require("../assets/images/carousel/5.png"),
+    require("../assets/images/carousel/6.png"),
+  ];
+
+
   return (
     <View
       style={{
@@ -12,7 +23,7 @@ export default function Index() {
       }}
     >
       <StatusBar style="auto"></StatusBar>
-      <PasswordStrengthMeter />
+      <Carousel images={images} />
     </View>
   );
 }
