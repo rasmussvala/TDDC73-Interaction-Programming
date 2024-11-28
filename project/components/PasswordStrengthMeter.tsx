459f324d9b255462d4242ab142e481865e5affa1
Grantallkotten <143391787+Grantallkotten@users.noreply.github.com>
2024-11-28 10:21:03 +0100

    Added color functunality for achived recomendation

--- project/components/PasswordStrengthMeter.tsx
+++ project/components/PasswordStrengthMeter.tsx
@@ -1,22 +1,21 @@
 import React, { useEffect, useState, useRef } from "react";
 import { View, Text, TextInput, StyleSheet } from "react-native";
 
-const ConfirmPasswordWidget = () => {
-  const [confirmPassword, setconfirmPassword] = useState("");
-
-  return (
-    <View>
-      <TextInput
-        style={styles.input}
-        placeholder="Confirm password"
-        value={confirmPassword}
-        onChangeText={setconfirmPassword}
-      />
-    </View>
-  );
+type PasswordStrengthMeterProps = {
+  nrOfChars?: number;
+  hasAtLeastOneUpperCase?: boolean;
+  hasAtLeastOneSpecialChar?: boolean;
+  hasAtLeastOneNumber?: boolean;
+  confirmPassword?: boolean;
 };
 
-const PasswordStrengthMeter = ({ nrOfCharacters = 8 }) => {
+const PasswordStrengthMeter = ({
+  nrOfChars = 8,
+  hasAtLeastOneUpperCase = false,
+  hasAtLeastOneSpecialChar = true,
+  hasAtLeastOneNumber = true,
+  confirmPassword = true,
+}: PasswordStrengthMeterProps) => {
   const [password, setPassword] = useState<string>("");
   const [strength, setStrength] = useState<number>(0.0);
   const [statusBarAColor, setStatusBarAColor] = useState<string>(colors.gray);
@@ -26,31 +25,56 @@ const PasswordStrengthMeter = ({ nrOfCharacters = 8 }) => {
   const textStatusArray = ["Weak", "Okay", "Good", "Strong"];
   const [textStatus, setTextStatus] = useState<string>(textStatusArray[0]);
 
-  const checkconfirmPassword = true;
+  const [achievementColors, setAchievementColors] = useState({
+    length: colors.darkGray,
+    upperCase: colors.darkGray,
+    specialChar: colors.darkGray,
+    number: colors.darkGray,
+  });
 
   const handlePasswordChange = (event: string) => {
     setPassword(event);
   };
 
   useEffect(() => {
+    const setFulfilledRecommendation = (
+      weightLength: number,
+      weightUpperCase: number,
+      weightSpecialChar: number,
+      weightNumber: number
+    ) => {
+      setAchievementColors({
+        length: weightLength >= 1.0 ? colors.green : colors.darkGray,
+        upperCase: weightUpperCase >= 1.0 ? colors.green : colors.darkGray,
+        specialChar: weightSpecialChar >= 1.0 ? colors.green : colors.darkGray,
+        number: weightNumber >= 1.0 ? colors.green : colors.darkGray,
+      });
+    };
+
     const getPasswordStrength = () => {
-      const weightLength = Math.min(password.length / nrOfCharacters, 1.0);
+      const weightLength = Math.min(password.length / nrOfChars, 1.0);
       const weightNumber = password.match(/[0-9]/) ? 1.0 : 0.0;
       const weightUpperCase = password.match(/[A-Z]/) ? 1.0 : 0.0;
-      const weightSpecialCharacter = /[^a-zA-Z0-9]/.test(password) ? 1.0 : 0.0;
+      const weightSpecialChar = /[^a-zA-Z0-9]/.test(password) ? 1.0 : 0.0;
 
-      // @TODO: REPLACE
-      const weightNumberOn = 1.0;
-      const weightUpperCaseOn = 1.0;
-      const weightSpecialCharacterOn = 1.0;
+      setFulfilledRecommendation(
+        weightLength,
+        weightUpperCase,
+        weightSpecialChar,
+        weightNumber
+      );
+
+      const weightUpperCaseOn = Number(hasAtLeastOneUpperCase);
+      const weightSpecialCharOn = Number(hasAtLeastOneSpecialChar);
+      const weightNumberOn = Number(hasAtLeastOneNumber);
 
       const num =
         weightLength +
-        weightNumber * weightNumberOn +
         weightUpperCase * weightUpperCaseOn +
-        weightSpecialCharacter * weightSpecialCharacterOn;
+        weightSpecialChar * weightSpecialCharOn +
+        weightNumber * weightNumber;
       const denom =
-        weightNumberOn + weightUpperCaseOn + weightSpecialCharacterOn + 1.0;
+        1.0 + weightUpperCaseOn + weightSpecialCharOn + weightNumberOn;
 
       return num / denom;
     };
@@ -93,7 +117,7 @@ const PasswordStrengthMeter = ({ nrOfCharacters = 8 }) => {
           onChangeText={(e) => handlePasswordChange(e)}
         />
       </View>
-      {checkconfirmPassword && <ConfirmPasswordWidget />}
+      {confirmPassword && <ConfirmPasswordWidget />}
       <View style={styles.status}>
         <View
           style={[styles.statusItem, { backgroundColor: statusBarAColor }]}
@@ -111,23 +135,59 @@ const PasswordStrengthMeter = ({ nrOfCharacters = 8 }) => {
       <Text style={styles.statusText}>{textStatus}</Text>
       <View style={styles.recommendContainer}>
         <Text style={styles.recommendHeader}>Recommended</Text>
-        <View style={styles.recommendation}>
-          <Text>✔</Text>
-          <Text style={styles.recommendText}>1 number</Text>
-        </View>
-        <View style={styles.recommendation}>
-          <Text>✔</Text>
-          <Text style={styles.recommendText}>1 upper case character</Text>
-        </View>
-        <View style={styles.recommendation}>
-          <Text>✔</Text>
-          <Text style={styles.recommendText}>1 special character</Text>
-        </View>
-        <View style={styles.recommendation}>
-          <Text>✔</Text>
-          <Text style={styles.recommendText}>{nrOfCharacters} characters</Text>
-        </View>
+        <RecommendationItem
+          text="1 number"
+          color={achievementColors.number}
+          achieved={hasAtLeastOneNumber}
+        />
+        <RecommendationItem
+          text="1 upper case Char"
+          color={achievementColors.upperCase}
+          achieved={hasAtLeastOneUpperCase}
+        />
+        <RecommendationItem
+          text="1 special Char"
+          color={achievementColors.specialChar}
+          achieved={hasAtLeastOneSpecialChar}
+        />
+        <RecommendationItem
+          text={`${nrOfChars} Chars`}
+          color={achievementColors.length}
+          achieved={true}
+        />
       </View>
+    </View>
+  );
+};
+
+const RecommendationItem = ({
+  text,
+  color,
+  achieved,
+}: {
+  text: string;
+  color: string;
+  achieved: boolean;
+}) => {
+  return achieved ? (
+    <View style={styles.recommendation}>
+      <Text style={[styles.recommendText, { color: color }]}>✔</Text>
+      <Text style={[styles.recommendText, { color: color }]}>{text}</Text>
+    </View>
+  ) : null;
+};
+
+const ConfirmPasswordWidget = () => {
+  const [confirmPassword, setconfirmPassword] = useState("");
+
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        placeholder="Confirm password"
+        value={confirmPassword}
+        onChangeText={setconfirmPassword}
+      />
     </View>
   );
 };
@@ -139,6 +199,7 @@ const colors = {
   red: "#e76f51",
   gray: "#d3d3d3",
   darkGray: "#808080",
+  black: "#000",
 };
 
 const styles = StyleSheet.create({
