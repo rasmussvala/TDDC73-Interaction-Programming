45d183961af714253eb2c6aeffcc6d88768ed694
Grantallkotten <143391787+Grantallkotten@users.noreply.github.com>
2024-12-05 10:16:51 +0100

    New images
    Co-Authored-By: Rasmus Svala <rasmus.svala@hotmail.com>

--- project/app/index.tsx
+++ project/app/index.tsx
@@ -5,12 +5,12 @@ import Carousel from "@/components/Carousel";
 
 export default function Index() {
   const images = [
-    require("../assets/images/carousel/1.png"),
-    require("../assets/images/carousel/2.png"),
-    require("../assets/images/carousel/3.png"),
-    require("../assets/images/carousel/4.png"),
-    require("../assets/images/carousel/5.png"),
     require("../assets/images/carousel/6.png"),
+    require("../assets/images/carousel/1.jpg"),
+    require("../assets/images/carousel/2.jpg"),
+    require("../assets/images/carousel/3.jpg"),
+    require("../assets/images/carousel/4.jpg"),
+    require("../assets/images/carousel/5.jpg"),
   ];
 
   return (
